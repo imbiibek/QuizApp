0714666a641099aacacfeff1623f7aad27e8eec1
imbiibek <bibekyadav735@gmail.com>
2024-10-16 13:27:57 +0545

    Register.java Updated

--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -1,6 +1,12 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Register extends AppCompatActivity {
+
+    public static final String Tag = "TAG";
+
+    EditText mFullname, mEmail, mPassword;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+
+    String UserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,53 @@ public class Register extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mFullname = findViewById(R.id.fullname);
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.Password);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mLoginBtn = findViewById(R.id.createText);
+
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              Intent intent = new Intent(Register.this, Login.class);
+              startActivity(intent);
+            }
+        });
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String fullname = mFullname.getText().toString();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password))
+                {
+                    mEmail.setError("Password is Required");
+                    return;
+                }
+
+                if(password.length()<6)
+                {
+                    mPassword.setError("Password must be greater than 6 characters");
+                    return;
+                }
+
+
+
+            }
+        });
+
+
     }
 }
