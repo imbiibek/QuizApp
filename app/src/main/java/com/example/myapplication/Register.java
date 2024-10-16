d44ccb4f30234e3f8a6c0f0666bccc19944c00d4
imbiibek <bibekyadav735@gmail.com>
2024-10-16 16:53:01 +0545

    Updated Register.java & Added Firebase, firestore, Authentication

--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -3,26 +3,45 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Register extends AppCompatActivity {
 
-    public static final String Tag = "TAG";
+    public static final String TAG = "TAG";
 
     EditText mFullname, mEmail, mPassword;
     Button mRegisterBtn;
     TextView mLoginBtn;
 
     String UserId;
+
+    FirebaseAuth fauth;
+    FirebaseFirestore fstore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +59,15 @@ public class Register extends AppCompatActivity {
         mPassword = findViewById(R.id.Password);
         mRegisterBtn = findViewById(R.id.registerBtn);
         mLoginBtn = findViewById(R.id.createText);
+
+        fauth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+        if (fauth.getCurrentUser() != null)
+        {
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
 
 
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
@@ -76,7 +104,61 @@ public class Register extends AppCompatActivity {
                     return;
                 }
 
+fauth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+    @Override
+    public void onComplete(@NonNull Task<AuthResult> task) {
 
+        if(task.isSuccessful()){
+
+            FirebaseUser fuser = fauth.getCurrentUser();
+            fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+
+                    Toast.makeText(Register.this, "Register Successful", Toast.LENGTH_SHORT).show();
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                    Log.d(TAG, "OnFailure: Email Not Sent" +e.getMessage());
+
+                }
+            });
+
+
+            Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
+            UserId = fauth.getCurrentUser().getUid();
+            DocumentReference documentReference = fstore.collection("users").document(UserId);
+            Map<String,Object> user = new HashMap<>();
+            user.put("fName", fullname);
+            user.put("email", email);
+            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Log.d(TAG, "onSuccess:user profile is created for" + UserId);
+
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                    Log.d(TAG, "onfailure: " +e.toString());
+                }
+            });
+
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+
+        else{
+
+            Toast.makeText(Register.this,"Error !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+});
 
             }
         });
