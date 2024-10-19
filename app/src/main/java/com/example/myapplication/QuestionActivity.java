39863417d4872fc2f691a758428ec147815b0b69
imbiibek <bibekyadav735@gmail.com>
2024-10-19 21:31:52 +0545

    activity_question.xml

--- app/src/main/java/com/example/myapplication/QuestionActivity.java
+++ app/src/main/java/com/example/myapplication/QuestionActivity.java
@@ -1,8 +1,6 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,39 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class QuestionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_question);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void C(View view) {
-
-        Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
-        startActivity(intent);
-
-    }
-
-    public void Cplus(View view) {
-    }
-
-    public void Python(View view) {
-    }
-
-    public void Html(View view) {
-    }
-
-    public void Kotlin(View view) {
-    }
-
-    public void Csharp(View view) {
     }
 }
