89613f487599c551bde6dc1712b786810707ecf7
imbiibek <bibekyadav735@gmail.com>
2024-10-20 21:05:44 +0545

    ResultActivity.java not complete yet

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ResultActivity extends AppCompatActivity {
+
+    TextView tv, tv2,tv3;
+    Button btnRestart;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,26 @@ public class ResultActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tv = (TextView) findViewById(R.id.tvres);
+        tv2 = (TextView) findViewById(R.id.tvres2);
+        tv3 = (TextView) findViewById(R.id.tvres3);
+
+        btnRestart = (Button) findViewById(R.id.btnRestart);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("Correct answer :" + QuestionActivity.correct + "\n");
+
+        StringBuffer sb2 = new StringBuffer();
+        sb2.append("Wrong answer :" + QuestionActivity.wrong  + "\n");
+
+        StringBuffer sb3 = new StringBuffer();
+        sb3.append("Final Score :" + QuestionActivity.correct + "\n");
+
+        tv.setText(sb);
+        tv2.setText(sb2);
+        tv3.setText(sb3);
+
+
     }
 }
